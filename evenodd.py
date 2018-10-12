c7f99734f19b3caa6e5835f0686ea0407e657e6d
abhishek8241 <44097685+abhishek8241@users.noreply.github.com>
2018-10-12 22:38:09 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,8 @@
+print("this program displays if a number is even or odd")
+a=int(input("enter a number : "))
+if(a==0):
+    print(a,"is neither even nor odd")
+elif(a%2==0):
+    print(a,"is even")
+else:
+    print(a,"is odd")
